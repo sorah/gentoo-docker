b6ac5afb3e50504cdef515e76faf14d0b47e6f08
Shota Fukumori (sora_h) <her@sorah.jp>
2015-01-11 09:56:12 +0900

    init

---
+++ stage3.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Import current stage3 then tagit
+
+set -x
+set -e
+
+MIRROR_PREFIX="${MIRROR_PREFIX:-ftp://ftp.iij.ad.jp/pub/linux/gentoo/releases/amd64/current-iso}"
+WORKDIR="/tmp/gentoo-docker-work-$(date +%s)"
+REPO="${1:-sorah/gentoo-docker-stage3}"
+
+mkdir $WORKDIR && cd $WORKDIR
+
+wget 'ftp://ftp.iij.ad.jp/pub/linux/gentoo/releases/amd64/current-iso/stage3-amd64-20*.tar.bz2'
+
+FILE="$(ls -1 stage3-*)"
+VERSION="$(ls -1 stage3-* | cut -d- -f 3 | cut -d. -f 1)"
+
+docker import - ${REPO}:${VERSION} < ${FILE}
+docker tag ${REPO}:${VERSION} ${REPO}:latest
+docker push ${REPO}:${VERSION}
+docker push ${REPO}:latest
