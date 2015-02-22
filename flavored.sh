faa159c17cb495db50f6110012eda175e78fa6bf
Shota Fukumori (sora_h) <her@sorah.jp>
2015-02-23 06:58:10 +0900

    Add flavored.sh

---
+++ flavored.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -x
+set -e
+
+REPO="${1:-quay.io/sorah/gentoo-flavored}"
+HEAD="$(git rev-parse HEAD)"
+VERSION="$(TZ=UTC date '+%Y%m%d%H%M%S')-${HEAD}"
+
+docker build -t ${REPO}:${VERSION} flavored
+
+docker tag -f ${REPO}:${VERSION} ${REPO}:${HEAD}
+docker tag -f ${REPO}:${VERSION} ${REPO}:latest
+
+exit
+
+docker push ${REPO}:${VERSION}
+docker push ${REPO}:${HEAD}
+docker push ${REPO}:latest
