1d9bb33bb9328a96f47bba9477521ffd8ac0b287
Shota Fukumori (sora_h) <her@sorah.jp>
2015-02-23 06:57:15 +0900

    skip existing version when SKIP_SAME is set

--- stage3.sh
+++ stage3.sh
@@ -4,6 +4,7 @@
 set -x
 set -e
 
+
 MIRROR_PREFIX="${MIRROR_PREFIX:-ftp://ftp.iij.ad.jp/pub/linux/gentoo/releases/amd64/autobuilds/current-stage3-amd64}"
 WORKDIR="/tmp/gentoo-docker-work-$(date +%s)"
 REPO="${1:-quay.io/sorah/gentoo-stage3}"
@@ -14,6 +15,14 @@ wget "${MIRROR_PREFIX}"'/stage3-amd64-20*.tar.bz2'
 
 FILE="$(ls -1 stage3-*)"
 VERSION="$(ls -1 stage3-* | cut -d- -f 3 | cut -d. -f 1)"
+
+if [ -n "${SKIP_SAME}" ]; then
+  if docker pull ${REPO}:${VERSION}; then
+    set +x
+    echo "already built, skipping"
+    exit
+  fi
+fi
 
 docker import - ${REPO}:${VERSION} < ${FILE}
 docker tag ${REPO}:${VERSION} ${REPO}:latest
